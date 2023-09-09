ef9295ea98351c4fb1542c3d38beca41c8175bba
Agnik Bakshi <bakshi.agnik@gmail.com>
2023-09-09 16:23:55 +0530

    Added Created Forgot Password

---
+++ frontend/src/components/Forgot.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Nav from './Nav';
+import './LoginStyle.css';
+export default function Forgot() {
+  return (
+    <>
+    <Nav/>
+    <div className="login">
+        <h1>Reset Password</h1>
+        <div className="email_div item">
+            <input type="email" name="email" id="email" placeholder='Enter Email'/>
+        </div>
+        <div className="password_div item">
+            <input type="password" name="password" id="new_password" placeholder='Enter new password'/>
+
+        </div>
+        <div className="confirm_div item">
+            <input type="password" name="reset_password" id="reset_password" placeholder='Confirm password'/>
+        </div>        
+        <div className="btn">
+            <button id="reset">Reset</button>
+        </div>
+    </div>
+    </>
+  )
+}
