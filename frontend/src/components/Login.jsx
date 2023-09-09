923db37d8c3127229eefe4a3be5eda4142a5789c
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-09-09 12:19:54 +0530

    login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope,faLock} from '@fortawesome/free-solid-svg-icons'
+import Nav from './Nav'
+import './LoginStyle.css'
 export default function Login() {
   return (
-    <div>
-      login
+    <>
+    <Nav/>
+    <div className="login">
+        <h1>Login</h1>
+        <div className="email_div item">
+            <input type="email" name="email" id="email" placeholder='Enter Email'/>
+            
+
+        </div>
+        <div className="password_div item">
+            <input type="password" name="password" id="password" placeholder='Enter password'/>
+
+        </div>
+        
+        <div className="forgot">
+            
+            <span className="new">Forgot Password?<span className="click"><a href="#">Click Here</a></span></span>
+        </div>
+        <div className="btn">
+            <button id="login">Login</button>
+        </div>
+        <div className="register">
+            Don't have an account? <span className="sign_up"><a href="#">Register</a></span>
+        </div>
+
     </div>
+    </>
   )
 }
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -12,7 +12,7 @@ export default function Nav() {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/login">Login/Register</Link></li>
             </ul>
         </nav>
     </header>
