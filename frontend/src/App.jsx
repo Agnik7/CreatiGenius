46cfaf25e7bf645bf7277e061aba934a3b14cbf6
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-09-09 10:24:51 +0530

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React,{ useState } from 'react';
 
 import About from "./components/About.jsx";
 import Login from "./components/Login.jsx";
-import Home from './components/home.jsx';
+import home from './components/home.jsx';
 import './App.css';
-
+import {Switch,Route,Redirect} from 'react-router-dom'
 
 function App() {
   
@@ -12,9 +12,19 @@ function App() {
   return (
     <>
     
-      <Login />
-      <About/>
-      <Home/>
+      
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<home/>}/>
+        </Routes>
+        <Routes>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+        <Routes>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+        
+      </BrowserRouter>
       
     </>
   );
