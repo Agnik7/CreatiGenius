070f2337257ff48156058a9b0d9da1cb19ff6fb0
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-09-09 10:55:58 +0530

    nav

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Nav() {
+  return (
+  
+
+    <div>
+      <header>
+        <nav>
+            <span className="name">Creatigenius</span>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        </nav>
+    </header>
+    </div>
+  )
+}
+
+ 
